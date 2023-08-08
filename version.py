2af97a4cd31e46e47ea29ce271e641daef0fdce1
malsaggaf <malsaggaf@stira.sa>
2023-08-08 15:32:26 +0300

    adding version file

--- version.py
+++ version.py
@@ -26,36 +26,57 @@ class GitVersion:
             if e.returncode == 1:
                 dirty = True
 
+        dirtyFiles = self._exec_git("status -s")
+
+
         # If WORKFLOW_BRANCH_OR_TAG is set in environment, is has precedence
         # (set by CI)
-        branch = (
-            os.environ.get("WORKFLOW_BRANCH_OR_TAG", None)
-            or self._exec_git("rev-parse --abbrev-ref HEAD")
-            or "unknown"
-        )
+        # branch = (
+        #     os.environ.get("WORKFLOW_BRANCH_OR_TAG", None)
+        #     or self._exec_git("rev-parse --abbrev-ref HEAD")
+        #     or "unknown"
+        # )
+
+
+        branch = self._exec_git("rev-parse --abbrev-ref HEAD")
+
 
         try:
-            version = self._exec_git("describe --tags --abbrev=0 --exact-match")
+            version = self._exec_git("describe --tags --abbrev=0") #latest version(tag) on the branch 
+            distanceFromTag = self._exec_git("describe --tags") #number of commmits between the tag and HEAD
+
+            try:
+                distanceFromTag = distanceFromTag.split('-')[1]
+
+            except IndexError:
+                distanceFromTag = '0'
+
+
         except subprocess.CalledProcessError:
             version = "unknown"
 
-        if "SOURCE_DATE_EPOCH" in os.environ:
-            commit_date = datetime.utcfromtimestamp(
-                int(os.environ["SOURCE_DATE_EPOCH"])
-            )
-        else:
-            commit_date = datetime.strptime(
-                self._exec_git("log -1 --format=%cd").strip(),
-                "%a %b %d %H:%M:%S %Y %z",
-            )
+        # if "SOURCE_DATE_EPOCH" in os.environ:
+        #     commit_date = datetime.utcfromtimestamp(
+        #         int(os.environ["SOURCE_DATE_EPOCH"])
+        #     )
+        # else:
+            # commit_date = datetime.strptime(
+            #     self._exec_git("log -1 --format=%cd").strip(),
+            #     "%a %b %d %H:%M:%S %Y %z",
+            # )
+
+        commit_date = self._exec_git("--no-pager show -s --date=local --format='%ad' --date=format:%d/%m/%Y-%H:%M:%S")
 
         return {
             "GIT_COMMIT": commit,
             "GIT_BRANCH": branch,
             "VERSION": version,
+            "TAG_DISTANCE_FROM_HEAD": distanceFromTag,
             "BUILD_DIRTY": dirty and 1 or 0,
+            "DIRTY_FILES": dirtyFiles,
             "GIT_ORIGIN": ",".join(self._get_git_origins()),
             "GIT_COMMIT_DATE": commit_date,
+
         }
 
     def _get_git_origins(self):
